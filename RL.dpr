ddbc67414d189eca85b900e2ac097e716cba1e3d
devapromix <bees@meta.ua>
2018-09-01 11:24:48 +0300

    Залил файлы.

---
+++ RL.dpr
@@ -0,0 +1,21 @@
+﻿program BearURQ;
+
+{$IFDEF FPC}
+{$IFDEF Windows}
+{$APPTYPE GUI}
+{$ENDIF}
+{$ENDIF}
+
+uses
+  SysUtils,
+  BearLibTerminal in 'sources\BearLibTerminal\BearLibTerminal.pas',
+  BearEngine.Terminal in 'sources\BearEngine.Terminal.pas';
+
+begin
+{$IFNDEF FPC}
+{$IF COMPILERVERSION >= 18}
+  ReportMemoryLeaksOnShutdown := True;
+{$IFEND}
+{$ENDIF}
+
+end.
